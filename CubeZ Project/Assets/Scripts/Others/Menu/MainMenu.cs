49f8abb338d89a0388d47fb55d26dc4e01264d82
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-04-12 14:53:28 +0500

    main menu UI (v1)

---
+++ CubeZ Project/Assets/Scripts/Others/Menu/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+    public class MainMenu : MonoBehaviour
+    {
+    [SerializeField] TextMeshProUGUI versionText;
+    // Use this for initialization
+    void Start()
+        {
+        if (versionText == null)
+        {
+            throw new MainMenuException("version text not seted");
+        }
+
+        versionText.text = Application.version;
+        }
+
+
+        public void Quit ()
+        {
+            Application.Quit();
+        }
+
+
+        public void SelectMap ()
+        {
+
+        }
+
+            public void ContinueSession ()
+        {
+
+        }
+
+        public void OpenSettings ()
+        {
+
+        }
+
+
+    }
---
+++ CubeZ Project/Assets/Scripts/Others/Menu/MainMenuException.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public class MainMenuException : System.Exception
+{
+    public MainMenuException() { }
+    public MainMenuException(string message) : base(message) { }
+    public MainMenuException(string message, System.Exception inner) : base(message, inner) { }
+    protected MainMenuException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
